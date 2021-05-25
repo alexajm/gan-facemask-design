92c03d0763f560f6c894f478912bd7c8521a1e80
Alex Maynard <amayn@stanford.edu>
2021-05-25 12:53:35 -0700

    Cleaned up codebase
    Cleaned up codebase by adding comments, moving Projector to gan.py, and adding common functions to utils.py.

--- code/gan.py
+++ code/gan.py
@@ -3,11 +3,12 @@ import torch
 from torch import nn, optim
 from facenet_pytorch import InceptionResnetV1
 import pdb
+import utils
 
 
 class Generator(nn.Module):
     def __init__(self, learning_rate=1e-3):
-        # initialize
+        '''Initialize generator for sampling facemask designs from latent space'''
         super(Generator, self).__init__()
         self.input_dim = 100
         self.output_dim = (128, 128, 3)
@@ -32,22 +33,115 @@ class Generator(nn.Module):
         self.criterion = nn.CrossEntropyLoss()
 
     def forward(self, latent_=None):
+        '''Generate a facemask design from the sampled latent space'''
         latent = latent_ if latent_ is not None else torch.rand(1, 100)
         layer1 = self.h1(latent)
         output = self.output(layer1)
         return torch.reshape(output, self.output_dim)
 
 
+class Projector(nn.Module):
+    def __init__(self, learning_rate=1e-3, load_path=None):
+        '''Initialize projector for projecting facemask designs onto images of individuals'''
+        super(Projector, self).__init__()
+
+        # hyperparameters
+        channels = [3, 6, 12, 18, 24, 1]
+
+        # layers
+        # input: 128x128 RGB image
+        # output: 128x128 transparency mask
+        self.h1 = nn.Sequential(
+            nn.Conv2d(in_channels=channels[0], out_channels=channels[1], kernel_size=3, padding=1),
+            nn.ReLU(),
+            )
+        self.h2 = nn.Sequential(
+            nn.Conv2d(in_channels=channels[1], out_channels=channels[2], kernel_size=3, padding=1),
+            nn.ReLU(),
+            )
+        self.h3 = nn.Sequential(
+            nn.Conv2d(in_channels=channels[2], out_channels=channels[3], kernel_size=3, padding=1),
+            nn.ReLU(),
+            )
+        self.h4 = nn.Sequential(
+            nn.Conv2d(in_channels=channels[3], out_channels=channels[4], kernel_size=3, padding=1),
+            nn.ReLU(),
+            )
+        self.output = nn.Sequential(
+            nn.Conv2d(in_channels=channels[4], out_channels=channels[5], kernel_size=3, padding=1),
+            nn.Sigmoid(),
+            )
+
+        # optimizer (Adam)
+        self.optimizer = optim.Adam(self.parameters(), learning_rate)
+
+        # loss (MSE)
+        self.criterion = nn.MSELoss()
+
+        # load prior weights
+        if load_path:
+            self.load_state_dict(torch.load(load_path))
+
+    def forward(self, x):
+        '''One forward propagation of the projector for a batch of images'''
+        y = self.h1(x)
+        y = self.h2(y)
+        y = self.h3(y)
+        y = self.h4(y)
+        y = self.output(y)
+        return y
+
+    def fit(self, inputs, outputs, num_epochs=10):
+        '''Train projector given masked faces as input and transparency masks as outputs'''
+        self.train()
+        return utils.fit(self, inputs, outputs, num_epochs=num_epochs)
+
+    def predict(self, x, process=False):
+        '''Predict transparency mask for faces not trained on'''
+        if len(x.shape) < 4:  # pre-process un-batched inputs
+            x = torch.unsqueeze(x, 0)
+        y = self.forward(x)
+        if process:  # post-process outputs to enhance mask quality
+            avg = torch.mean(y)
+            y = (y > avg).type(torch.IntTensor)
+        return y
+
+    def evaluate(self, inputs, correct_outputs):
+        '''Evaluate loss of projector for given inputs'''
+        # set model to eval mode
+        self.eval()
+
+        # forward propagation
+        batch_outputs = self.forward(inputs)
+
+        # return test loss
+        loss = self.criterion(batch_outputs.float(), correct_outputs.float())
+        n_examples = correct_outputs.shape[0]
+        return float(loss) / n_examples
+
+    def project_mask(self, facemask, masked_faces, process=False):
+        '''Project a facemask design onto images of masked individuals'''
+        transparency_masks = self.predict(masked_faces, process=process)
+        if facemask.shape[2] == 3:
+            facemask = torch.transpose(facemask, 2, 1)
+            facemask = torch.transpose(facemask, 1, 0)
+        facemask = facemask.unsqueeze(0)
+        generator_faces = (masked_faces + .5) * transparency_masks + facemask * (1 - transparency_masks)
+        return generator_faces
+
+
 class Discriminator(nn.Module):
     def __init__(self, learning_rate=1e-3):
-        # initialize
+        '''Initialize discriminator as pre-trained facial recognition model'''
         super(Discriminator, self).__init__()
         self.model = InceptionResnetV1(pretrained='vggface2').eval()
 
     def forward(self, img):
+        '''One forward propagation of the discriminator for a batch of images'''
         return self.model(img)
 
     def evaluate(self, query, target):
+        '''Evaluate classification accuracy of discriminator for given inputs'''
         # get embeddings
         target_embeddings = self.forward(target)
         query_embeddings = self.forward(query)
@@ -87,16 +181,16 @@ class GAN():
         '''Compute and store database of embeddings for unmasked faces'''
         self.unmasked_embeddings = self.discriminator(unmasked_faces)
 
+    def generate_mask(self):
+        '''Generate a facemask design'''
+        return self.generator()
+
     def project_mask(self, facemask, masked_faces, process=False):
-        transparency_masks = self.projector.predict(masked_faces, process=process)
-        if facemask.shape[2] == 3:
-            facemask = torch.transpose(facemask, 2, 1)
-            facemask = torch.transpose(facemask, 1, 0)
-        facemask = facemask.unsqueeze(0)
-        generator_faces = (masked_faces + .5) * transparency_masks + facemask * (1 - transparency_masks)
-        return generator_faces
+        '''Project a facemask design onto images of masked individuals'''
+        return self.projector.project_mask(facemask, masked_faces, process=process)
 
     def one_hot(self, ids):
+        '''Generate one-hot vectors for given ids based on pre-computed embeddings'''
         classes = torch.zeros((self.unmasked_embeddings.shape[0], len(ids)))
         for i in range(classes.shape[0]):
             classes[i,ids[i]] = 1
@@ -111,75 +205,20 @@ class GAN():
         generator_faces = self.project_mask(mask, masked_faces) - .5
 
         # get embeddings from discriminator
-        # masked_embeddings = self.discriminator(masked_faces)
         generator_embeddings = self.discriminator(generator_faces)
 
         # determine distances from known, unmasked embeddings
-        # masked_distances = np.linalg.norm(batch_unmasked_embeddings - masked_embeddings)  # NOTE: may need axis argument
-        # pdb.set_trace()
         difference = self.unmasked_embeddings.unsqueeze(0) - generator_embeddings.unsqueeze(1)
         generator_distances = torch.linalg.norm(difference, dim=2)
-
-        # determine identity of masked faces
-        # masked_identities = np.argmax(masked_distances)
-        generator_identities = torch.argmax(generator_distances)
-
-        # return softmax of distances
-        # masked_softmax = nn.functional.softmax(masked_distances, dim=1)
-        # generator_softmax = nn.functional.softmax(generator_distances, dim=1)
         return generator_distances
 
-        # return discriminator's classification accuracies
-        # NOTE: need to be returning something the generator can train on, e.g. softmax probs?
-        # return np.mean(masked_identities == masked_ids), np.mean(generator_identities == masked_ids)
-
-    def shuffle_data(self, inputs, outputs):
-        n_examples = outputs.shape[0]
-        shuffled_indices = torch.randperm(n_examples)
-        inputs = inputs[shuffled_indices,:,:,:]
-        outputs = outputs[shuffled_indices]
-        return inputs, outputs
-
-    def batch_data(self, inputs, outputs, batch_size=16):
-        n_examples = outputs.shape[0]
-        return [ (inputs[batch_size * i:batch_size * (i+1),:,:,:],
-                  outputs[batch_size * i:batch_size * (i+1)])
-                 for i in range(n_examples // batch_size) ]
-
-    def train_batch(self, batch):
-        inputs, correct_ids = batch
-        generator_outputs = self.forward(inputs)
-        self.optimizer.zero_grad()
-        # pdb.set_trace()
-        loss = self.criterion(generator_outputs, correct_ids)
-        loss.backward(retain_graph=True)
-        self.optimizer.step()
-        return float(loss) / generator_outputs.shape[0]
-
     def fit(self, inputs, ids, num_epochs=10):
-        # set model to train mode
+        '''Train GAN given masked faces as input and their ids as labels'''
         self.generator.train()
-
-        # train over desired number of epochs
-        epoch_loss = 0
-        for epoch in range(1, num_epochs + 1):
-            # sort data into minibatches
-            inputs, ids = self.shuffle_data(inputs, ids)
-            minibatches = self.batch_data(inputs, ids)
-
-            # train on each minibatch
-            epoch_loss = 0
-            for batch in minibatches:
-                epoch_loss += self.train_batch(batch)
-            epoch_loss /= len(minibatches)
-
-            # output loss
-            if epoch % 10 == 0: print('Epoch {} loss: {}'.format(epoch, epoch_loss))
-
-        # return training accuracy
-        return epoch_loss
+        return utils.fit(self, inputs, ids, num_epochs=num_epochs)
 
     def evaluate(self, inputs, correct_classes):
+        '''Evaluate classification accuracy of GAN for given inputs'''
         # set model to eval mode
         self.generator.eval()
 
---
+++ code/utils.py
@@ -0,0 +1,50 @@
+import torch
+
+
+def shuffle_data(inputs, outputs):
+    '''Shuffle the first dimension of a set of input/output data'''
+    n_examples = outputs.shape[0]
+    shuffled_indices = torch.randperm(n_examples)
+    inputs = inputs[shuffled_indices]
+    outputs = outputs[shuffled_indices]
+    return inputs, outputs
+
+
+def batch_data(inputs, outputs, batch_size=16):
+    '''Convert full input/output pairs to a list of batched tuples'''
+    n_examples = outputs.shape[0]
+    return [ (inputs[batch_size * i:batch_size * (i+1)],
+              outputs[batch_size * i:batch_size * (i+1)])
+             for i in range(n_examples // batch_size) ]
+
+
+def train_batch(model, batch):
+    '''Perform one iteration of model training given a single batch'''
+    inputs, correct_outputs = batch
+    model_outputs = model.forward(inputs)
+    model.optimizer.zero_grad()
+    loss = model.criterion(model_outputs, correct_outputs)
+    loss.backward()
+    model.optimizer.step()
+    return float(loss) / model_outputs.shape[0]
+
+
+def fit(model, inputs, correct_outputs, num_epochs=10):
+    ''' Train model on input/output pairs over desired number of epochs'''
+    epoch_loss = 0
+    for epoch in range(1, num_epochs + 1):
+        # sort data into minibatches
+        inputs, correct_outputs = shuffle_data(inputs, correct_outputs)
+        minibatches = batch_data(inputs, correct_outputs)
+
+        # train on each minibatch
+        epoch_loss = 0
+        for batch in minibatches:
+            epoch_loss += train_batch(model, batch)
+        epoch_loss /= len(minibatches)
+
+        # output loss
+        if epoch % 10 == 0: print('Epoch {} loss: {}'.format(epoch, epoch_loss))
+
+    # return training accuracy
+    return epoch_loss
